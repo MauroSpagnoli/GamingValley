d2d5b4f8c37465c78d2353afc8843fc3708b7705
MauroSpagnoli <maurospagnoliherrera@hotmail.com>
2017-01-30 11:59:51 +0100

    CrearPDF_Codigo_Actualizacion
    Clase para crear PDFs para la aplicación. Se ha de añadir la librería adjunta al proyecto.

---
+++ Codigo_GuardarPDF.java
@@ -0,0 +1,34 @@
+/*
+ * 
+ * 
+ * 
+ */
+package pdfcreator;
+
+/**
+ *
+ * @author mauro
+ */
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfWriter;
+public class PDFCreator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws FileNotFoundException, DocumentException {
+      FileOutputStream archivo = new FileOutputStream("C:\\Users\\mauro\\Desktop\\Nueva carpeta\\Hola.pdf");
+      Document documento = new Document();
+      PdfWriter.getInstance(documento, archivo);
+      documento.open();
+      documento.add(new Paragraph("Hola Mundo!"));
+      documento.add(new Paragraph("SoloInformaticaYAlgoMas.blogspot.com"));
+      documento.close();
+    }
+    
+}
