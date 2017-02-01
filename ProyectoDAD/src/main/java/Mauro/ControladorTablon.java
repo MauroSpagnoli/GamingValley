d76fa5762aa9f4fff448a33c89be502304dc34ea
Miguel Robledo <migueljrd@hotmail.com>
2017-02-01 18:39:53 +0100

    nothing

--- ProyectoDAD/src/main/java/Mauro/ControladorTablon.java
+++ ProyectoDAD/src/main/java/Mauro/ControladorTablon.java
@@ -2,14 +2,11 @@ package Mauro;
 import javax.annotation.PostConstruct;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.Banner.Mode;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class ControladorTablon{
 
