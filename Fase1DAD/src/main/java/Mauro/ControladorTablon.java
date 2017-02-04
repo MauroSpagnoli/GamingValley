426709988f7d1c7ebe0581b32580ae6d23a0ed6f
MauroSpagnoli <maurospagnoliherrera@hotmail.com>
2017-02-04 18:20:32 +0100

    Update

--- Fase1DAD/src/main/java/Mauro/ControladorTablon.java
+++ Fase1DAD/src/main/java/Mauro/ControladorTablon.java
@@ -80,9 +80,11 @@ public class ControladorTablon{
 			repositorioNoticias.save(noticia);
 			return "noticia_guardada";
 		}
-		@PostMapping("videojuego{{id}}/nueva_valoracion")
-		public String agregarValoracion(Model model, @PathVariable long id, Valoracion valoracion, Usuario usuario){
-			Usuario usuarioEncontrado = repositorioUsuarios.findOne(usuario.getId());
+		@PostMapping("videojuego/{id}/nueva_valoracion")
+		public String agregarValoracion(Model model, @PathVariable long id, Valoracion valoracion){
+			Videojuego videojuego = this.repositorioVideojuegos.findOne(id);
+			model.addAttribute("id",id);
+			Usuario usuarioEncontrado = repositorioUsuarios.getOne(1L);
 			valoracion.setAutor(usuarioEncontrado);
 			repositorioValoraciones.save(valoracion);
 			return "valoracion_guardada";
