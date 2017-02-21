bf7ace2be45e0ad2eb96f7d4c2f84504ea302ea7
Miguel Robledo <migueljrd@hotmail.com>
2017-02-21 13:48:17 +0100

    clean warnings

--- Fase1DAD/src/main/java/Mauro/Pedido.java
+++ Fase1DAD/src/main/java/Mauro/Pedido.java
@@ -1,17 +1,10 @@
 package Mauro;
-
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
-
-import org.hibernate.annotations.Cascade;
-
 import java.util.ArrayList;
 import java.util.List;
 
